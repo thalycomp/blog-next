0a3ad0291c402887bb8a4524692c94900d67d9ab
thalyta <thalycomp@gmail.com>
2021-03-23 16:46:27 -0300

    excluindo componente nao utilizado

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,15 +3,14 @@ import { GlobalStyles } from '../styles/global';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Container from '../components/Container';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Container>
+    <>
       <GlobalStyles />
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </Container>
+    </>
   )
 }
